2c838a95cd79f4e8363c6c8b952fa45d83ee8fee
Mathew <maddaugh1@outlook.com>
2019-03-12 00:13:37 -0700

    js udate

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -18,53 +18,34 @@ function displayMovieInfo() {
             console.log(queryURL);
 
             console.log(response);
-      // Creating a div to hold the gif
-      var movieDiv = $("<div class='gif'>");
+            var results = response.data;
+            // Creating a div to hold the gif
+            for (var i = 0; i < results.length; i++) {
 
-      // Storing the rating data
-      var rating = response.Rated;
+                // Creating and storing a div tag
+                var gifDiv = $("<div>");
 
-      // Creating an element to have the rating displayed
-      var pOne = $("<p>").text("Rating: " + rating);
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + results[i].rating);
 
-      // Displaying the rating
-      movieDiv.append(pOne);
+                // Creating and storing an image tag
+                var gifImage = $("<img>");
+                // Setting the src attribute of the image to a property pulled off the result item
+                gifImage.attr("src", results[i].images.fixed_height.url);
 
-      // Storing the release year
-      var released = response.Released;
+                // Appending the paragraph and image tag to the gifDiv
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
 
-      // Creating an element to hold the release year
-      var pTwo = $("<p>").text("Released: " + released);
+                // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
+                $("#gif-view").prepend(gifDiv);
+            }
+        });
 
-      // Displaying the release year
-      movieDiv.append(pTwo);
+}
 
-      // Storing the plot
-      var plot = response.Plot;
-
-      // Creating an element to hold the plot
-      var pThree = $("<p>").text("Plot: " + plot);
-
-      // Appending the plot
-      movieDiv.append(pThree);
-
-      // Retrieving the URL for the image
-      var imgURL = response.Poster;
-
-      // Creating an element to hold the image
-      var image = $("<img>").attr("src", imgURL);
-
-      // Appending the image
-      movieDiv.append(image);
-
-      // Putting the entire gif above the previous movies
-      $("#movies-view").prepend(movieDiv);
-    });
-
-  }
-
-  // Function for displaying gif data
-  function renderButtons() {
+// Function for displaying gif data
+function renderButtons() {
 
     // Deleting the movies prior to adding new movies
     // (this is necessary otherwise you will have repeat buttons)
@@ -73,22 +54,22 @@ function displayMovieInfo() {
     // Looping through the array of movies
     for (var i = 0; i < gifs.length; i++) {
 
-      // Then dynamicaly generating buttons for each gif in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var a = $("<button>");
-      // Adding a class of gif-btn to our button
-      a.addClass("gif-btn");
-      // Adding a data-attribute
-      a.attr("data-name", gifs[i]);
-      // Providing the initial button text
-      a.text(gifs[i]);
-      // Adding the button to the buttons-view div
-      $("#buttons-view").append(a);
+        // Then dynamicaly generating buttons for each gif in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of gif-btn to our button
+        a.addClass("gif-btn");
+        // Adding a data-attribute
+        a.attr("data-name", gifs[i]);
+        // Providing the initial button text
+        a.text(gifs[i]);
+        // Adding the button to the buttons-view div
+        $("#buttons-view").append(a);
     }
-  }
+}
 
-  // This function handles events where a gif button is clicked
-  $("#add-gif").on("click", function(event) {
+// This function handles events where a gif button is clicked
+$("#add-gif").on("click", function (event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var gif = $("#gif-input").val().trim();
@@ -98,10 +79,10 @@ function displayMovieInfo() {
 
     // Calling renderButtons which handles the processing of our gif array
     renderButtons();
-  });
+});
 
-  // Adding a click event listener to all elements with a class of "gif-btn"
-  $(document).on("click", ".gif-btn", displayMovieInfo);
+// Adding a click event listener to all elements with a class of "gif-btn"
+$(document).on("click", ".gif-btn", displayMovieInfo);
 
-  // Calling the renderButtons function to display the intial buttons
-  renderButtons();
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
