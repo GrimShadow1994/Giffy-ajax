0177ed552f69317806e0e2da131a7346f49fac06
Mathew <maddaugh1@outlook.com>
2019-03-12 00:21:54 -0700

    js udate

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -8,81 +8,56 @@ function displayMovieInfo() {
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=Woe8iVBWAKGEpRHcxIIpyd0R9VU31Z1z&limit=10";
 
-    // Performing an AJAX request with the queryURL
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-        // After data comes back from the request
         .then(function (response) {
             console.log(queryURL);
 
             console.log(response);
             var results = response.data;
-            // Creating a div to hold the gif
             for (var i = 0; i < results.length; i++) {
 
-                // Creating and storing a div tag
                 var gifDiv = $("<div>");
 
-                // Creating a paragraph tag with the result item's rating
                 var p = $("<p>").text("Rating: " + results[i].rating);
 
-                // Creating and storing an image tag
                 var gifImage = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
                 gifImage.attr("src", results[i].images.fixed_height.url);
 
-                // Appending the paragraph and image tag to the gifDiv
                 gifDiv.append(p);
                 gifDiv.append(gifImage);
 
-                // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
                 $("#gif-view").prepend(gifDiv);
             }
         });
 
 }
 
-// Function for displaying gif data
 function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
 
-    // Looping through the array of movies
     for (var i = 0; i < gifs.length; i++) {
 
-        // Then dynamicaly generating buttons for each gif in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
-        // Adding a class of gif-btn to our button
         a.addClass("gif-btn");
-        // Adding a data-attribute
         a.attr("data-name", gifs[i]);
-        // Providing the initial button text
         a.text(gifs[i]);
-        // Adding the button to the buttons-view div
         $("#buttons-view").append(a);
     }
 }
 
-// This function handles events where a gif button is clicked
 $("#add-gif").on("click", function (event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
     var gif = $("#gif-input").val().trim();
 
-    // Adding gif from the textbox to our array
     gifs.push(gif);
 
-    // Calling renderButtons which handles the processing of our gif array
     renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "gif-btn"
 $(document).on("click", ".gif-btn", displayMovieInfo);
 
-// Calling the renderButtons function to display the intial buttons
 renderButtons();
